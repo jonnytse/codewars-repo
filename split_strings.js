7a599a4c8e20e8d592bbe232c85334937c40d357
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-05-20 21:03:18 +0100

    split strings WET solution

--- split_strings.js
+++ split_strings.js
@@ -9,33 +9,26 @@
 // solution('abcdef') // should return ['ab', 'cd', 'ef']
 
 
-// const solution = (string) => {
-//     if (string.length % 2 === 0) {
-//         return (string.match(/.{1,2}/g));    
-//     } else {
-//         return (string.match(/.{1,2}/g).concat('_'))
-//     }
-// }
-// console.log(solution('qwertyi'));
-// // ... this only works for even-numbered strings...and references stack overflow 'How can I split a string into segments of n characters?'
 
 
+
+//***** My horrible and long-assed version to pass all the tests. Jee-ZUS.
 
 const solution = (string) => {
     let solutionArray = []
     let stringSplit = string.split('')
     let lastCharacter = stringSplit[string.length - 1]
-    if(stringSplit.length !== 0) {
+    if(stringSplit.length % 2 !== 0) {
        for (let i = 0; i < stringSplit.length - 1; i += 2) {
-           solutionArray.push(stringSplit[i].concat(stringSplit[i + 1]));
+           solutionArray.push(stringSplit[i].concat(stringSplit[i + 1]))
        }
-       solutionArray.push(lastCharacter.concat('_'));
+       solutionArray.push(lastCharacter.concat('_'))
     } else {
-        solutionArray.pop() // this code should remove the final element of the solutionArray in an even-lengthed string, but doesn't?
-    }   
+        for (let i = 0; i < stringSplit.length; i += 2) {
+            solutionArray.push(stringSplit[i].concat(stringSplit[i + 1]))
+        }
+    }    
     return solutionArray;
-}
-console.log(solution('qwerty'));
-
-// this only works for odd-numbered string length!
+   }
+   console.log(solution('qwerty'));
  
